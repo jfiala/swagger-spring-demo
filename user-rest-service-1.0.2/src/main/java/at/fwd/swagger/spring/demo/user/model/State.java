44c7026d7a81db29629fd99fc8b5544900c72d19
Johannes Fiala <johannes.fiala@fwd.at>
2015-04-10 13:14:20 +0200

    Initial setup

--- user-rest-service-1.0.2/src/main/java/at/fwd/swagger/spring/demo/user/model/State.java
+++ user-rest-service-1.0.2/src/main/java/at/fwd/swagger/spring/demo/user/model/State.java
@@ -1,11 +1,14 @@
 package at.fwd.swagger.spring.demo.user.model;
 
+import com.wordnik.swagger.annotations.ApiModel;
+
 /**
  * state to demo enum
  * 
  * @author johannes.fiala@fwd.at
  *
  */
+@ApiModel(value="User", description="User object")
 public enum State {
 
 	ACTIVE, DISABLED;
